e98a449d114fb1f6612af63476620961b64db744
Miles Pratt <miles@Miless-MacBook-Pro.local>
2020-11-16 21:16:21 -0500

    add scanning, remove buttons

--- engine/app.js
+++ engine/app.js
@@ -5,13 +5,14 @@ import { screenWidth, screenHeight } from "../config";
 
 // TODO: Take in config as parameters
 export function createApp(height, width, viewportId) {
-  const newApp = new PIXI.Application({
-    width: width || screenWidth,
-    height: height || screenHeight,
-    transparent: true,
-    antialias: true,
-    autoResize: true,
-    resizeTo: document.getElementById(viewportId),
-  });
-  return newApp;
+	const newApp = new PIXI.Application({
+		width: width || screenWidth,
+		height: height || screenHeight,
+		transparent: true,
+		antialias: true,
+		autoResize: true,
+		resizeTo: document.getElementById(viewportId),
+		// resolution: 2,
+	});
+	return newApp;
 }
