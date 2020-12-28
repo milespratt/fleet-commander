df52a36d86a692e856bc719fb5f243c0dcee3794
Miles Pratt <miles@thoughtpowder.com>
2020-12-28 12:15:04 -0500

    Add window hiding

--- src/config/controls.js
+++ src/config/controls.js
@@ -3,30 +3,52 @@ const controls = {
     {
       name: "Information",
       controls: [
-        { name: "Sector", icon: "fa-vector-square" },
-        { name: "Solar System", icon: "fa-solar-system" },
-        { name: "Star", icon: "fa-sun" },
-        { name: "Planet", icon: "fa-planet-moon" },
-        { name: "Ship", icon: "fa-starfighter" },
+        { name: "Sector", windowID: "sector-window", icon: "fa-vector-square" },
+        {
+          name: "Solar System",
+          windowID: "solar-system-window",
+          icon: "fa-solar-system",
+        },
+        { name: "Star", windowID: "star-window", icon: "fa-sun" },
+        { name: "Planet", windowID: "planet-window", icon: "fa-planet-moon" },
+        { name: "Ship", windowID: "ship-window", icon: "fa-starfighter" },
+        {
+          name: "Encyclopedia",
+          windowID: "encyclopedia-window",
+          icon: "fa-book",
+        },
       ],
     },
     {
       name: "Communication",
       controls: [
-        { name: "Chat", icon: "fa-comments-alt" },
-        { name: "Mail", icon: "fa-mail-bulk" },
-        { name: "Users", icon: "fa-users" },
-        { name: "Profile", icon: "fa-user" },
+        { name: "Chat", windowID: "chat-window", icon: "fa-comments-alt" },
+        { name: "Mail", windowID: "mail-window", icon: "fa-mail-bulk" },
+        { name: "Users", windowID: "users-window", icon: "fa-users" },
+        { name: "Profile", windowID: "profile-window", icon: "fa-user" },
       ],
     },
-    {
-      name: "Utility",
-      controls: [
-        { name: "Toggle Windows", icon: "fa-border-all" },
-        { name: "Toggle Grid", icon: "fa-window-restore" },
-        { name: "Encyclopedia", icon: "fa-book" },
-      ],
-    },
+    //     {
+    //       name: "Utility",
+    //       controls: [
+    //         { name: "Toggle Grid", icon: "fa-border-all" },
+    //         {
+    //           name: "Toggle Windows",
+    //           icon: "fa-window-restore",
+    //         },
+    //         {
+    //           name: "Encyclopedia",
+    //           windowID: "encyclopedia-window",
+    //           icon: "fa-book",
+    //         },
+    //         { name: "Report Bug", icon: "fa-bug" },
+    //         {
+    //           name: "Request Feature",
+    //
+    //           icon: "fa-flask",
+    //         },
+    //       ],
+    //     },
   ],
 };
 
--- src/loadControls.js
+++ src/loadControls.js
@@ -1,22 +1,82 @@
 import controls from "./config/controls";
 
+function toggleWindow(control, windowElement) {
+  if (control.classList.contains("control--faded")) {
+    control.classList.remove("control--faded");
+    if (windowElement) {
+      windowElement.classList.remove("faded");
+    }
+  } else {
+    control.classList.add("control--faded");
+    if (windowElement) {
+      windowElement.classList.add("faded");
+    }
+  }
+}
+
 function makeControl(control) {
-  return `<button class="control global bordered blue__glow padded--light">
-	  <i class="fal ${control.icon}"></i>
-	  <span class="global__label">${control.name}</span>
-	</button>`;
+  const windowElement = document.getElementById(control.windowID);
+  let windowCloseButton;
+  if (windowElement) {
+    windowElement.classList.add("faded");
+    windowCloseButton = document.getElementById(`${control.windowID}-close`);
+  }
+  const newControl = document.createElement("BUTTON");
+  const classes = [
+    "control",
+    "control--faded",
+    "global",
+    "bordered",
+    "blue__glow",
+    "padded--light",
+    // "control--notify",
+  ];
+  newControl.classList.add(...classes);
+  newControl.innerHTML = ` <i class="fal ${control.icon}"></i>
+	<span class="global__label">${control.name}</span>`;
+  if (windowElement) {
+    newControl.addEventListener("click", () => {
+      toggleWindow(newControl, windowElement);
+    });
+
+    windowCloseButton.addEventListener("click", () => {
+      toggleWindow(newControl, windowElement);
+    });
+  }
+  return newControl;
+}
+
+function makeControlGroup(group, container) {
+  const newGroupClasses = ["control__group", "grid", "grid--r"];
+  const newGroup = document.createElement("DIV");
+  newGroup.classList.add(...newGroupClasses);
+
+  const newGroupControlsClasses = [
+    "control__group__controls",
+    "grid",
+    "grid--c",
+  ];
+  const newGroupControls = document.createElement("DIV");
+  newGroupControls.classList.add(...newGroupControlsClasses);
+
+  const newGroupLabelClasses = ["control__group__label", "blue__glow"];
+  const newGroupLabel = document.createElement("SPAN");
+  newGroupLabel.innerText = group.name;
+  newGroupLabel.classList.add(...newGroupLabelClasses);
+
+  newGroup.appendChild(newGroupControls);
+  newGroup.appendChild(newGroupLabel);
+
+  group.controls.forEach((control) => {
+    newGroupControls.appendChild(makeControl(control));
+  });
+  container.appendChild(newGroup);
 }
 
 export default () => {
   const globalContainer = document.getElementById("globals");
+  let windows;
   controls.categories.forEach((category) => {
-    globalContainer.innerHTML += `
-	  <div class="control__group grid grid--r">
-	  <div class="control__group__controls grid grid--c">
-	    ${category.controls.map((control) => makeControl(control)).join("")}
-	  </div>
-	  <span class="control__group__label blue__glow">${category.name}</span>
-	</div>
-	  `;
+    makeControlGroup(category, globalContainer);
   });
 };
